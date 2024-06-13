dac8a69c34d6c8b2b7be5009a340867658063691
rushi-1525 <rushisatani152530@gmail.com>
2024-06-13 10:29:15 +0530

    first commit

---
+++ index.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+#include<string.h>
+
+int main(){
+
+// Q-1
+
+// char a='a';
+// do
+// {
+//     printf("%c",a);
+//     a=a+4;
+// } while (a<='z');
+
+
+// Q=2
+
+//  int a;
+//  int count=0;
+
+//  printf("Enter any number:");
+//  scanf("%d",&a);
+
+//  while (a!=0)
+//  {
+//      a=a/10;
+//      count++;
+//  }
+//  printf("Count is %d\n",count);
+  
+
+
+// Q=3
+
+
+// int num;
+// int first,last,sum;
+// printf("enter a number : ");
+// scanf("%d",&num);
+// last=num%10;
+// while(num!=0){
+//     first=num;
+//     num=num/10;
+// }
+// sum=first+last;
+// printf("sum of first and last digit is %d\n",sum);
+
+    return 0;
+}
